b6d5e68c6d95f5885222e4bf45e012eadfa98a10
OlgaSirenko <132263673+CamillaSirenko@users.noreply.github.com>
2023-11-27 20:29:06 +0300

    my_commit

--- hm10_myproject/hm10_myproject/urls.py
+++ hm10_myproject/hm10_myproject/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('quotes.urls'))
-    path('user_auth/', include('user_auth.urls'))
+    path('', include('quotes.urls')),
+    path('users/', include('users.urls'))
 ]
